3b73249a249bd662c3f9f1852f391237bfac6132
Claire Villard <60586848+claire-villard-sonarsource@users.noreply.github.com>
2024-03-19 03:59:54 -0600

    SONAR-21859 Create a GitHub Action to analyze C/C++ code with SonarQube

---
+++ scripts/configure_paths.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [[ ${ARCH} != "X64" ]]; then 
+  echo "::error::Architecture '${ARCH}' is unsupported by build-wrapper"
+  exit 1
+fi
+
+case ${OS} in
+  Windows)
+    SONAR_SCANNER_SUFFIX="windows"
+    BUILD_WRAPPER_SUFFIX="win-x86"
+    SONAR_SCANNER_NAME="sonar-scanner.bat"
+    BUILD_WRAPPER_NAME="build-wrapper-win-x86-64.exe"
+    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_WINDOWS}"
+    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_WINDOWS}"
+    ;;
+  Linux)  
+    SONAR_SCANNER_SUFFIX="linux"
+    BUILD_WRAPPER_SUFFIX="linux-x86"
+    SONAR_SCANNER_NAME="sonar-scanner"
+    BUILD_WRAPPER_NAME="build-wrapper-linux-x86-64"
+    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX}"
+    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX}"
+    ;;
+  macOS)
+    SONAR_SCANNER_SUFFIX="macosx"
+    BUILD_WRAPPER_SUFFIX="macosx-x86"
+    SONAR_SCANNER_NAME="sonar-scanner"
+    BUILD_WRAPPER_NAME="build-wrapper-macosx-x86"
+    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_MACOSX}"
+    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_MACOSX}"
+    ;;
+  *)
+    echo "::error::Unsupported runner OS '${OS}'"
+    exit 1
+    ;;
+esac
+
+
+echo "sonar-scanner-url=${SONAR_SCANNER_URL}"
+echo "sonar-scanner-sha=${SONAR_SCANNER_SHA}"
+
+SONAR_SCANNER_DIR="${INSTALL_PATH}/sonar-scanner-${SONAR_SCANNER_VERSION}-${SONAR_SCANNER_SUFFIX}"
+echo "sonar-scanner-dir=${SONAR_SCANNER_DIR}"
+echo "sonar-scanner-bin=${SONAR_SCANNER_DIR}/bin/${SONAR_SCANNER_NAME}"
+
+BUILD_WRAPPER_DIR="${INSTALL_PATH}/build-wrapper-${BUILD_WRAPPER_SUFFIX}"
+echo "build-wrapper-url=${SONAR_HOST_URL}/static/cpp/build-wrapper-${BUILD_WRAPPER_SUFFIX}.zip"
+echo "build-wrapper-dir=${BUILD_WRAPPER_DIR}"
+echo "build-wrapper-bin=${BUILD_WRAPPER_DIR}/${BUILD_WRAPPER_NAME}"
+
