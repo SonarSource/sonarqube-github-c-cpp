4aebde3df73d6b8588d2b61acc360cd94185da2c
Vincenzo Pellegrini <vincenzo.pellegrini@sonarsource.com>
2024-06-24 17:54:15 +0200

    SQCPPGHA-5 Support Running on Linux ARM64 using platform dependent scanner cli

--- scripts/configure_paths.sh
+++ scripts/configure_paths.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-if [[ ${ARCH} != "X64" && ! ( ${OS} == "macOS" && ${ARCH} == "ARM64" ) ]]; then 
+if [[ ${ARCH} != "X64" && ! (${ARCH} == "ARM64" && (${OS} == "macOS" || ${OS} == "Linux")) ]]; then
   echo "::error::Architecture '${ARCH}' is unsupported by build-wrapper"
   exit 1
 fi
 
 case ${OS} in
   Windows)
-    SONAR_SCANNER_SUFFIX="windows"
+    SONAR_SCANNER_SUFFIX="windows-x64"
     BUILD_WRAPPER_SUFFIX="win-x86"
     SONAR_SCANNER_NAME="sonar-scanner.bat"
     BUILD_WRAPPER_NAME="build-wrapper-win-x86-64.exe"
-    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_WINDOWS}"
-    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_WINDOWS}"
+    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_WINDOWS_X64}"
+    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_WINDOWS_X64}"
     ;;
   Linux)  
-    SONAR_SCANNER_SUFFIX="linux"
-    BUILD_WRAPPER_SUFFIX="linux-x86"
+    case ${ARCH} in
+      X64)
+        SONAR_SCANNER_SUFFIX="linux-x64"
+        BUILD_WRAPPER_SUFFIX="linux-x86"
+        BUILD_WRAPPER_NAME="build-wrapper-linux-x86-64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX_X64}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX_X64}"
+        ;;
+      ARM64)
+        SONAR_SCANNER_SUFFIX="linux-aarch64"
+        BUILD_WRAPPER_SUFFIX="linux-aarch64"
+        BUILD_WRAPPER_NAME="build-wrapper-linux-aarch64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX_AARCH64}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX_AARCH64}"
+        ;;
+    esac
     SONAR_SCANNER_NAME="sonar-scanner"
-    BUILD_WRAPPER_NAME="build-wrapper-linux-x86-64"
-    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX}"
-    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX}"
     ;;
   macOS)
-    SONAR_SCANNER_SUFFIX="macosx"
+    case ${ARCH} in
+      X64)
+        SONAR_SCANNER_SUFFIX="macosx-x64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_MACOSX_X64}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_MACOSX_X64}"
+        ;;
+      ARM64)
+        SONAR_SCANNER_SUFFIX="macosx-aarch64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_MACOSX_AARCH64}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_MACOSX_AARCH64}"
+        ;;
+    esac
     BUILD_WRAPPER_SUFFIX="macosx-x86"
     SONAR_SCANNER_NAME="sonar-scanner"
     BUILD_WRAPPER_NAME="build-wrapper-macosx-x86"
-    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_MACOSX}"
-    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_MACOSX}"
     ;;
   *)
     echo "::error::Unsupported runner OS '${OS}'"
