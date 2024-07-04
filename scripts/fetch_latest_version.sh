62ddc31b4bb689fefc73e2c1ad549375d79d5a7c
Vincenzo Pellegrini <vincenzo.pellegrini@sonarsource.com>
2024-07-04 11:33:11 +0200

    Update sonar-scanner-version to 6.1.0.4477

--- scripts/fetch_latest_version.sh
+++ scripts/fetch_latest_version.sh
@@ -9,10 +9,17 @@ check_status "Failed to fetch latest sonar-scanner version from GitHub API"
 echo "sonar-scanner-version=${SONAR_SCANNER_VERSION}"
 
 for OS in windows linux macosx; do
-  SONAR_SCANNER_URL="https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-${OS}.zip"
-  SONAR_SCANNER_SHA=$(curl -sSL "${SONAR_SCANNER_URL}.sha256")
-  check_status "Failed to download ${OS} sonar-scanner checksum from '${SONAR_SCANNER_URL}'"
+  if [[ "$OS" == "windows" ]]; then
+    ARCHS=("x64")
+  else
+    ARCHS=("x64" "aarch64")
+  fi
+  for ARCH in "${ARCHS[@]}"; do
+    SONAR_SCANNER_URL="https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-${OS}-${ARCH}.zip"
+    SONAR_SCANNER_SHA=$(curl -sSL "${SONAR_SCANNER_URL}.sha256")
+    check_status "Failed to download ${OS} ${ARCH} sonar-scanner checksum from '${SONAR_SCANNER_URL}'"
 
-  echo "sonar-scanner-url-${OS}=${SONAR_SCANNER_URL}"
-  echo "sonar-scanner-sha-${OS}=${SONAR_SCANNER_SHA}"
+    echo "sonar-scanner-url-${OS}-${ARCH}=${SONAR_SCANNER_URL}"
+    echo "sonar-scanner-sha-${OS}-${ARCH}=${SONAR_SCANNER_SHA}"
+  done
 done
